55c070a3886214ff8d791104000d1ad089cee743
guychien <guychienll@gmail.com>
2020-04-22 07:54:53 +0800

    Updates

---
+++ precache-manifest.597f36e2299057f8cd04cc633daefa0e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb1b642dde7d2aafd7cb0e16960e326b",
+    "url": "/react-gh-pages-demo/index.html"
+  },
+  {
+    "revision": "53a2f981f7058d1a8353",
+    "url": "/react-gh-pages-demo/static/css/main.38e1b73e.chunk.css"
+  },
+  {
+    "revision": "9c70e1dbce027c0cb4ec",
+    "url": "/react-gh-pages-demo/static/js/2.3e6ba633.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-gh-pages-demo/static/js/2.3e6ba633.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53a2f981f7058d1a8353",
+    "url": "/react-gh-pages-demo/static/js/main.620e7a88.chunk.js"
+  },
+  {
+    "revision": "2e10b474a1e3d4aab6a8",
+    "url": "/react-gh-pages-demo/static/js/runtime-main.41537ee7.js"
+  }
+]);
